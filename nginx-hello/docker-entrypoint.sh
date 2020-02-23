3d8db2dc97115d0afbcb43130406d1db1c5ce313
Aaron Vinson <avinson.public@gmail.com>
2020-02-23 14:15:47 -0800

    updates

---
+++ nginx-hello/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -n "$WAIT_FOR_WEBHOOK" ]; then
+  WEBHOOK_CONTAINER_NAME=webhook
+  until docker logs ${WEBHOOK_CONTAINER_NAME} | grep -q "Connect to .*localhost.run" ; do
+    >&2 echo "webhook unavailable - sleeping"
+    sleep 1
+  done
+  export WEBHOOK_URI=$(docker logs ${WEBHOOK_CONTAINER_NAME} | grep "Connect to .*localhost.run" | grep -Po 'https://.*')
+fi
+
+dockerize -template /etc/nginx/conf.d/hello-plain-text.conf.tmpl:/etc/nginx/conf.d/hello-plain-text.conf
+echo "executing $@"
+exec "$@"
