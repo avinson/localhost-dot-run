30dc1bb0fb54b1ad05d4e4e9aacaa4a46b52e14b
Aaron Vinson <avinson.public@gmail.com>
2020-02-24 16:29:58 -0800

    regex improvement

--- nginx-hello/docker-entrypoint.sh
+++ nginx-hello/docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ -n "$WAIT_FOR_WEBHOOK" ]; then
     >&2 echo "webhook unavailable - sleeping"
     sleep 1
   done
-  export WEBHOOK_URI=$(docker logs ${WEBHOOK_CONTAINER_NAME} | tr -d '[:cntrl:]' | grep 'Connect to .*localhost.run' | grep -Po 'https://.*$')
+  export WEBHOOK_URI=$(docker logs ${WEBHOOK_CONTAINER_NAME} | tr -d '[:cntrl:]' | grep 'Connect to .*localhost.run' | grep -Po 'https://.*\.run$')
 fi
 
 dockerize -template /etc/nginx/conf.d/hello-plain-text.conf.tmpl:/etc/nginx/conf.d/hello-plain-text.conf
