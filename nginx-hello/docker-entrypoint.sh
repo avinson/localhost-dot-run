1252bc4ac0b5ba599d5a3635e0504754508035b6
Aaron Vinson <avinson.public@gmail.com>
2020-11-09 12:10:04 -0800

    update example nginx container to grep for current localhost.run output

--- nginx-hello/docker-entrypoint.sh
+++ nginx-hello/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ -n "$WAIT_FOR_WEBHOOK" ]; then
-  until docker logs ${WEBHOOK_CONTAINER_NAME} | grep -q 'Connect to .*localhost.run' ; do
+  until docker logs ${WEBHOOK_CONTAINER_NAME} | grep -q 'tunneled with tls termination' ; do
     >&2 echo "webhook unavailable - sleeping"
     sleep 1
   done
-  export WEBHOOK_URI=$(docker logs ${WEBHOOK_CONTAINER_NAME} | grep 'Connect to .*localhost.run' | grep -Po 'https://.*\.run' | tr -d '[:cntrl:]')
+  export WEBHOOK_URI=https://$(docker logs ${WEBHOOK_CONTAINER_NAME} | grep 'tunneled with tls termination' | grep -Po "${SSH_HOSTUSER}.*\.run" | tr -d '[:cntrl:]')
   echo "WEBHOOK_URI is ${WEBHOOK_URI}"
 fi
 
